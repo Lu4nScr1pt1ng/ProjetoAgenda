93c074dd3c946d8e8522aa09d6766f86c5a1484a
Lu4nScr1pt1ng <rd1qys@gmail.com>
2022-05-12 23:22:23 -0300

    add contacts function / display contacts in index from db / new routes(edit, delete)/ login session db

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/LoginModel')
 
 exports.index = (req, res) => {
-    res.render('login');
+    if(req.session.user) return res.render('login-logado');
+    return res.render('login');
   };
   
 exports.register = async function(req, res) {
@@ -17,7 +18,7 @@ exports.register = async function(req, res) {
             return;
         }
 
-        req.flash('success', 'Seu usuario fo criado com sucesso')
+        req.flash('success', 'Seu usuario foi criado com sucesso')
         req.session.save(function () {
             return res.redirect('/login/index')
         });
@@ -27,3 +28,33 @@ exports.register = async function(req, res) {
     }
 
 };
+
+exports.login = async function(req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+    
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors)
+            req.session.save(function () {
+                return res.redirect('/login/index')
+            });
+            return;
+        }
+
+        req.flash('success', 'Logado com sucesso.');
+        req.session.user = login.user;
+        req.session.save(function () {
+            return res.redirect('/login/index')
+        });
+    } catch(e) {
+        console.log(e);
+        res.render('404')
+    }
+
+};
+
+exports.logout = function(req, res) {
+    req.session.destroy();
+    res.redirect('/');
+}
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -16,6 +16,26 @@ class Login {
     this.user = null;
   }
 
+  async login() {
+    this.valida();
+
+    if(this.errors.length > 0) return;
+
+    this.user = await LoginModel.findOne({ email: this.body.email});
+
+    if(!this.user) {
+      this.errors.push('Usuário inexistente');
+      return;
+    };
+
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+      this.errors.push('Senha inválida');
+      this.user = null;
+      return;
+    }
+
+  }
+
   async register() {
     this.valida();
 
@@ -29,16 +49,14 @@ class Login {
     this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
 
-    try {
-      this.user = await LoginModel.create(this.body);
-    } catch(e) {
-      console.log(e);
-    }
+
+    this.user = await LoginModel.create(this.body);
+
   }
 
   async userExists() {
-    const user = await LoginModel.findOne({ email: this.body.email});
-    if(user) this.errors.push('Usuário já existe na base de dados');
+    this.user = await LoginModel.findOne({ email: this.body.email});
+    if(this.user) this.errors.push('Usuário já existe na base de dados');
   }
   
 
